bb45d04d8b9f59d338118e2230b5ab20e4e56195
Nicole Lambon <Takaya213@users.noreply.github.com>
2023-02-08 14:44:29 +0200

    Hero fix

--- src/components/icons.js
+++ src/components/icons.js
@@ -1,5 +1,7 @@
 import { ReactComponent as FwdArrow } from '../assets/images/arrow-forward.svg'
+import { ReactComponent as GirlDesk } from '../assets/images/girl-at-desk.svg'
 
 export default {
-    FwdArrow
+    FwdArrow,
+    GirlDesk
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,14 +5,19 @@ import Icon from '../components/icons'
 
 function Home() {
   return (
-    <div className="hero-image">
-      <div className='hero-text'>
-        <p>Nicole Lambon</p>
-        <h1>UI Designer <br />&amp; Developer</h1>
-        <Link to='/' className='button button-dark button-arrow'>
-          <span>Start a project</span>
-          <Icon.FwdArrow  style={{ color: '#FFFFFF' }} />
-        </Link>
+    <div className="hero-wrapper">
+      <div className="hero-area">
+        <div className='hero-text'>
+          <p>Nicole Lambon</p>
+          <h1>UI Designer <br />&amp; Developer</h1>
+          <Link to='/' className='button button-dark button-arrow'>
+            <span>Start a project</span>
+            <Icon.FwdArrow  style={{ color: '#FFFFFF' }} />
+          </Link>
+        </div>
+        <div className='hero-image'>
+          <Icon.GirlDesk />
+        </div>
       </div>
     </div>
   );
