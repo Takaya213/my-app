a992b665cc8f6eae1448e83b269f4838bbc069e2
Nicole Lambon <Takaya213@users.noreply.github.com>
2023-02-09 15:22:11 +0200

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,25 @@
 import React from "react"
+import { Link } from 'react-router-dom'
+
+import Icon from './icons'
 
 function Footer() {
   return (
     <footer>
-      This is the footer.
+      <div className='footer-wrapper'>
+        <div>
+          <h2>Turn your brand <br />into a great one.</h2>
+          <Link to='/' className='button button-light button-arrow'>
+            <span>Let's talk</span>
+            <Icon.FwdArrow style={{ color: '#121212' }} />
+          </Link>
+        </div>
+      </div>
+      <div className='footer-wrapper'>
+        <div className='copyright'>
+          <p>{new Date().getFullYear()} &copy; Copyright Nicole Lambon</p>
+        </div>
+      </div>
     </footer>
   )
 }
