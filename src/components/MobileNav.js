0216c84c8e85ca74f43c8e1d1be18b0d1e21cc49
Nicole Lambon <Takaya213@users.noreply.github.com>
2023-02-24 16:53:40 +0200

    Mobile nav close on link click

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -1,27 +1,39 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from 'react-router-dom'
 import { slide as Menu } from 'react-burger-menu'
 import { Link as Url } from 'react-scroll'
 
 import Icon from './Icons'
 
-function MobileNav() {
+const MobileNav = () => {
+  const [isMenuOpen, handleMenu] = useState(false);
+  const handleCloseMenu = () => {
+    handleMenu(false);
+  };
+  const handleStateChange = (state) => {
+    handleMenu(state.isOpen);
+  };
   return (
-    <Menu right noOverlay>
-        <ul>
-          <li><Url activeClass="active" to='about' spy={true} smooth={true} offset={0} duration={500}>About</Url></li>
-          <li><Url activeClass="active" to='work' spy={true} smooth={true} offset={0} duration={500}>Work</Url></li>
-          <li><Url activeClass="active" to='experience' spy={true} smooth={true} offset={0} duration={500}>Experience</Url></li>
-          <li><Url activeClass="active" to='skills' spy={true} smooth={true} offset={0} duration={500}>Skills</Url></li>
-          <li><Url activeClass="active" to='education' spy={true} smooth={true} offset={0} duration={500}>Education</Url></li>
-          <li><Url activeClass="active" to='journal' spy={true} smooth={true} offset={0} duration={500}>Journal</Url></li>
-          <li>
-              <Link to='/' className='button button-dark-border button-arrow'>
-                  <span>Let's talk</span>
-                  <Icon.FwdArrow style={{ color: '#FFFFFF' }} />
-              </Link>
-          </li>
-        </ul>
+    <Menu 
+      right
+      noOverlay
+      isOpen={isMenuOpen}
+      onStateChange={handleStateChange}
+    >
+      <ul>
+        <li><Url activeClass="active" to='about' spy={true} smooth={true} offset={0} duration={500} onClick={() => handleCloseMenu()}>About</Url></li>
+        <li><Url activeClass="active" to='work' spy={true} smooth={true} offset={0} duration={500} onClick={() => handleCloseMenu()}>Work</Url></li>
+        <li><Url activeClass="active" to='experience' spy={true} smooth={true} offset={0} duration={500} onClick={() => handleCloseMenu()}>Experience</Url></li>
+        <li><Url activeClass="active" to='skills' spy={true} smooth={true} offset={0} duration={500} onClick={() => handleCloseMenu()}>Skills</Url></li>
+        <li><Url activeClass="active" to='education' spy={true} smooth={true} offset={0} duration={500} onClick={() => handleCloseMenu()}>Education</Url></li>
+        <li><Url activeClass="active" to='journal' spy={true} smooth={true} offset={0} duration={500} onClick={() => handleCloseMenu()}>Journal</Url></li>
+        <li>
+            <Link to='/' className='button button-dark-border button-arrow'>
+                <span>Let's talk</span>
+                <Icon.FwdArrow style={{ color: '#FFFFFF' }} />
+            </Link>
+        </li>
+      </ul>
     </Menu>
   )
 }
