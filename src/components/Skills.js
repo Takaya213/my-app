848cce45a381ee4dc71e127b3f9a995eb85e3127
Nicole Lambon <Takaya213@users.noreply.github.com>
2023-02-24 15:48:38 +0200

    Home page sections + Favicon

---
+++ src/components/Skills.js
@@ -0,0 +1,31 @@
+import React from "react"
+
+function Skills() {
+    return (
+        <div className='wrapper inner-padding'>
+            <h2>Skills.</h2>
+            <div className='inner-wrapper skills'>
+                <div>
+                    <p>Design</p>
+                    <p>Figma (Design and Prototype)<br />
+                        User Flows<br />
+                        Information Architecture UX<br />
+                        Sitemaps<br />
+                        Accessibility</p>
+                </div>
+                <div>
+                    <p>Development</p>
+                    <p>HTML 5<br />
+                        CSS (Sass, XStyled)<br />
+                        jQuery<br />
+                        Craft CMS<br />
+                        Accessibility<br />
+                        Visual Studio Code<br />
+                        Visual Studio</p>
+                </div>
+            </div>
+        </div>
+  )
+}
+
+export default Skills
