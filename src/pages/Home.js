e3df625ceee310ddffa343642e617333d94e5090
Nicole Lambon <Takaya213@users.noreply.github.com>
2023-03-01 09:54:32 +0200

    Button Component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,28 @@
-import React from "react"
+import React from 'react'
 import { Element } from 'react-scroll'
 
-import PageTitle from "../components/PageTitle"
+import PageTitle from '../components/PageTitle'
 import Icon from '../components/Icons'
-import Services from "../components/Services"
-import Work from "../components/Work"
-import Experience from "../components/Experience"
-import Skills from "../components/Skills"
-import Education from "../components/Education"
-import StartProject from "../components/StartProject"
+import Services from '../components/Services'
+import Work from '../components/Work'
+import Experience from '../components/Experience'
+import Skills from '../components/Skills'
+import Education from '../components/Education'
+import Button from '../components/Button'
 
 function Home() {
   return (
     <PageTitle title='Welcome'>
-      <Element name="about">
-        <div className="hero-wrapper">
-          <div className="hero-area">
+      <Element name='about'>
+        <div className='hero-wrapper'>
+          <div className='hero-area'>
             <div className='hero-text'>
               <p>Nicole Lambon</p>
-              <h1>UI Designer <br />&amp; Developer</h1>
-              <StartProject />
+              <h1>
+                UI Designer <br />
+                &amp; Developer
+              </h1>
+              <Button styleClass='button-dark' text='Start a Project' colour='#FFFFFF' />
             </div>
             <div className='hero-image'>
               <Icon.GirlDesk />
@@ -27,23 +30,23 @@ function Home() {
           </div>
         </div>
       </Element>
-      <Element name="services">
+      <Element name='services'>
         <Services />
       </Element>
-      <Element name="work">
+      <Element name='work'>
         <Work />
       </Element>
-      <Element name="experience">
+      <Element name='experience'>
         <Experience />
       </Element>
-      <Element name="skills">
+      <Element name='skills'>
         <Skills />
       </Element>
-      <Element name="education">
+      <Element name='education'>
         <Education />
       </Element>
     </PageTitle>
-  );
+  )
 }
 
 export default Home
