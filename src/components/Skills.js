6f3603f0345e85fc1da722277f4f23ea215f4956
Nicole Lambon <Takaya213@users.noreply.github.com>
2023-03-10 08:38:02 +0200

    Skills Strapi

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function Skills() {
+const [error, setError] = useState(null);
+  const [skills, setSkills] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:1337/api/skills')
+      .then(({ data }) => setSkills(data))
+      .catch(error => setError(error))
+  }, [])
+
+  if (error) {
+    // Print errors if any
+    return <div>An error occured: {error.message}</div>;
+  }
   return (
     <div className='wrapper inner-padding'>
       <h2>Skills.</h2>
+      {skills.map(({ id, skilltype }) => <li key={id}>{skilltype}</li>)}
       <div className='inner-wrapper skills'>
         <div>
           <p>Design</p>
