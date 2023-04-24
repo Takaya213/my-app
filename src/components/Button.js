784647a3640b31a27c46508c5c82b09ef76a9019
Nicole Lambon <Takaya213@users.noreply.github.com>
2023-04-24 09:20:17 +0200

    Revert "Button + Modal fix"
    This reverts commit 701117fc1ee8ecac009f733f9516611d38c97c6f.

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,20 +1,6 @@
 import React, { useState } from 'react'
-import Modal from 'react-modal'
 
-import ContactForm from './ContactForm'
 import Icon from './Icons'
-
-const customStyles = {
-  content: {
-    width: '100vw',
-    height: '100vh',
-    top: '0',
-    left: '0',
-    bottom: '0',
-    right: '0',
-    display: 'flex'
-  }
-}
 
 function Button(props) {
   const [modalIsOpen, setIsOpen] = useState(false)
@@ -23,31 +9,11 @@ function Button(props) {
     setIsOpen(true)
     document.body.style.overflow = 'hidden'
   }
-
-  function closeModal() {
-    setIsOpen(false)
-    document.body.style.overflow = 'unset'
-  }
-
   return (
-    <>
-      <button onClick={openModal} className={'button button-arrow ' + props.styleClass}>
-        <span>{props.text}</span>
-        <Icon.FwdArrow style={{ color: props.colour }} />
-      </button>
-      <Modal isOpen={modalIsOpen} onRequestClose={closeModal} style={customStyles} contentLabel='Lets Talk'>
-        <div className='contact'>
-          <button className='close-btn' onClick={closeModal}>
-            <span>Close</span>
-            <Icon.CloseBtn />
-          </button>
-          <h2>Let's Talk.</h2>
-          <div>
-            <ContactForm />
-          </div>
-        </div>
-      </Modal>
-    </>
+    <button onClick={openModal} className={'button button-arrow ' + props.styleClass}>
+      <span>{props.text}</span>
+      <Icon.FwdArrow style={{ color: props.colour }} />
+    </button>
   )
 }
 
